ac475bd6ffe1e2f3eabda1bbcb47e581eb6efe0a
robinhawiz <Robin.Hawiz.6670@student.uu.se>
2026-04-18 15:32:24 +0200

    fix(app): handle Fastify errors in global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
-import Fastify from "fastify";
+import Fastify, { type FastifyError } from "fastify";
 import { fastifyAwilixPlugin } from "@fastify/awilix";
 import cors from "@fastify/cors";
 import cookie from "@fastify/cookie";
 import { AppError } from "@errors/appError.js";
 import { SchemaValidationError } from "@errors/schemaValidationError.js";
+
+function isFastifyError(err: unknown): err is FastifyError {
+  return (
+    typeof err === "object" &&
+    err !== null &&
+    "code" in err &&
+    "name" in err &&
+    err.name === "FastifyError" &&
+    "statusCode" in err
+  );
+}
 
 export default async function build() {
   const app = Fastify({ logger: true });
@@ -42,6 +53,20 @@ export default async function build() {
     } else if (err instanceof SchemaValidationError) {
       app.log.warn(err);
       reply.code(err.statusCode).send({ message: err.message });
+    } else if (isFastifyError(err)) {
+      if (
+        err.statusCode !== undefined &&
+        err.statusCode >= 400 &&
+        err.statusCode < 500
+      ) {
+        app.log.warn(err);
+        reply.code(err.statusCode).send({
+          message: err.message,
+        });
+        return;
+      }
+      app.log.error(err);
+      reply.code(err.statusCode ?? 500).send({ message: err.message });
     } else {
       app.log.error(err);
       reply.code(500).send({ message: "Unexpected server error" });
